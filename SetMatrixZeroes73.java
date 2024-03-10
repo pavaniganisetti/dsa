2d70cec6ec8dc4eb1c949a299df889677439fc36
pavaniganisetti <74003181+pavaniganisetti@users.noreply.github.com>
2024-03-10 12:09:01 +0530

    Update SetMatrixZeroes73.java
    /**
    Time complexity: O(m*n(m+n))
    Space complexity: O(m*n)
    */

--- SetMatrixZeroes73.java
+++ SetMatrixZeroes73.java
@@ -1,8 +1,52 @@
 
+/**
+Time complexity: O(m*n(m+n))
+Space complexity: O(m*n)
+*/
+
+class Solution {
+    public void setZeroes(int[][] arr) {
+        
+        int rowLen = arr.length;
+        int colLen = arr[0].length;;
+
+        int[][]  ans = new int[rowLen][colLen];
+
+        for (int i=0;i<rowLen;i++){
+            for (int j=0;j<colLen;j++) {
+                ans[i][j] = 1;
+            }
+        }
+
+        for (int i=0;i<rowLen;i++){
+            for (int j=0;j<colLen;j++) {
+                if (arr[i][j] == 0) {
+                    
+                    for (int k = 0; k < rowLen; k++)
+                        ans[k][j] = 0; 
+                    for (int k = 0; k <colLen;k++)
+                        ans[i][k] = 0;
+
+                }
+            }
+        }
+
+        for (int i=0;i<rowLen;i++){
+            for (int j=0;j<colLen;j++) {
+                if (ans[i][j] == 0)
+                    arr[i][j] = ans[i][j];
+            }
+        }
+
+    }
+}
 
 
 
-// Space complexity O(m+n)
+/**
+Space complexity O(m+n)
+Time complexity O(m*n)
+*/
 
 class Solution {
     public void setZeroes(int[][] arr) {
